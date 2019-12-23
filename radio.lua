43d5e80a0cce23a162ce0d5d670611b4e8e90bc8
FinnCoding <finndpaes@gmail.com>
2019-12-23 20:58:13 +0100

    First commit
    First upload with required files.

---
+++ radio.lua
@@ -0,0 +1,22 @@
+local radioStatus = 0
+
+function OnKeyPress(key)
+    if key == "Q" then
+        local inVehicle = IsPlayerInVehicle()
+        if inVehicle == true then
+            if radioStatus == 1 then
+                AddPlayerChat("Radio is turning off...")
+                DestroySound(track)
+                radioStatus = 0
+            else
+                AddPlayerChat("Radio is turning on...")
+                AddPlayerChat("Before you are getting of the vehicle press Q again.")
+                AddPlayerChat("If the song stopped, press Q again to start a new song.")
+                local tracknumber = Random(1,3)
+                track = CreateSound("track"..tracknumber..".mp3")
+                radioStatus = 1
+            end
+        end
+	end
+end
+AddEvent("OnKeyPress", OnKeyPress)
