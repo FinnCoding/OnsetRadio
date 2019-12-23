a88ce9de3b35f53b07e740d779aae444d52d30fa
FinnCoding <finndpaes@gmail.com>
2019-12-23 21:00:33 +0100

    Update radio.lua

--- radio.lua
+++ radio.lua
@@ -12,7 +12,7 @@ function OnKeyPress(key)
                 AddPlayerChat("Radio is turning on...")
                 AddPlayerChat("Before you are getting of the vehicle press Q again.")
                 AddPlayerChat("If the song stopped, press Q again to start a new song.")
-                local tracknumber = Random(1,3)
+                local tracknumber = Random(1,3) -- Generates random number, make sure to edit this number to how much tracks u have.
                 track = CreateSound("track"..tracknumber..".mp3")
                 radioStatus = 1
             end
